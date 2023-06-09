6324f87119665362a954e45914d605625d2f59ff
IslemBenAlaya <135045921+IslemBenAlaya@users.noreply.github.com>
2023-06-09 15:04:44 -0400

    islem

--- script.js
+++ script.js
@@ -1,73 +1,31 @@
 $(document).ready(function() {
   // Initialize the map
-  var map = L.map('map').setView([56.1304, -106.3468], 4);
+  var map = L.map('map', {
+    zoomControl: false // Disable the default zoom control
+  }).setView([56.1304, -106.3468], 4);
 
-  // Add a modern map tile provider (Mapbox)
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    maxZoom: 19,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      'Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiaXNsZW1iZW5hbGF5YSIsImEiOiJjbGlhdDllc2cwNXczM2VuYmQ0ZWhqd3NsIn0.FDi7R8iChso-iD81WmBB3Q' // Replace with your Mapbox access token
+  // Add a custom zoom control
+  L.control.zoom({
+    position: 'topright' // Set the position of the zoom control
   }).addTo(map);
 
-  // Create the layers (not added to the map initially)
-  var bellLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:Bell Canada',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Bell Canada'
-  });
+  // Add OpenStreetMap as the tile provider
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: Infinity, // Modify the maximum zoom limit
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
-  var cityWestLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:CITY WEST CABLE AND TELEPHONE CORP',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'CITY WEST CABLE AND TELEPHONE CORP'
-  });
+  // Create the bellLayer with onEachFeature function to display popups
+  var bellLayer = L.geoJSON().addTo(map);
 
-  var cogecoLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:Cogeco',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Cogeco'
-  });
-
-  var eastlinkLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:Eastlink',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Eastlink'
-  });
-
-  var ecotelLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:Ecotel inc',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Ecotel inc'
-  });
-
-  var execulinkLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:EXECULINK TELECOM INC',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'EXECULINK TELECOM INC'
-  });
-
-  var sasktelLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:Sasktel',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Sasktel'
-  });
-
-  var sogetelLayer = L.tileLayer.wms('http://localhost:8080/geoserver/CellularSites/wms', {
-    layers: 'CellularSites:Sogetel',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Sogetel'
+// Load the GeoJSON data from GitHub repository
+fetch('https://raw.githubusercontent.com/IslemBenAlaya/CellularTours/main/Bell%20Canada.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    bellLayer.addData(data);
+    map.fitBounds(bellLayer.getBounds()); // Adjust the map bounds to fit the GeoJSON layer
   });
 
   // Function to toggle a layer
@@ -87,55 +45,100 @@ $(document).ready(function() {
     toggleLayer(bellLayer, button);
   }
 
-  // Function to toggle the City West Cable and Telephone Corp layer
-  function toggleCityWestLayer() {
-    var button = document.getElementById("toggleCityWestButton");
-    toggleLayer(cityWestLayer, button);
-  }
-
-  // Function to toggle the Cogeco layer
-  function toggleCogecoLayer() {
-    var button = document.getElementById("toggleCogecoButton");
-    toggleLayer(cogecoLayer, button);
-  }
-
-  // Function to toggle the Eastlink layer
-  function toggleEastlinkLayer() {
-    var button = document.getElementById("toggleEastlinkButton");
-    toggleLayer(eastlinkLayer, button);
-  }
-
-  // Function to toggle the Ecotel layer
-  function toggleEcotelLayer() {
-    var button = document.getElementById("toggleEcotelButton");
-    toggleLayer(ecotelLayer, button);
-  }
-
-  // Function to toggle the Execulink Telecom Inc layer
-  function toggleExeculinkLayer() {
-    var button = document.getElementById("toggleExeculinkButton");
-    toggleLayer(execulinkLayer, button);
-  }
-
-  // Function to toggle the Sasktel layer
-  function toggleSasktelLayer() {
-    var button = document.getElementById("toggleSasktelButton");
-    toggleLayer(sasktelLayer, button);
-  }
-
-  // Function to toggle the Sogetel layer
-  function toggleSogetelLayer() {
-    var button = document.getElementById("toggleSogetelButton");
-    toggleLayer(sogetelLayer, button);
-  }
-
   // Attach click event listeners to the buttons
   document.getElementById("toggleBellButton").addEventListener("click", toggleBellLayer);
-  document.getElementById("toggleCityWestButton").addEventListener("click", toggleCityWestLayer);
-  document.getElementById("toggleCogecoButton").addEventListener("click", toggleCogecoLayer);
-  document.getElementById("toggleEastlinkButton").addEventListener("click", toggleEastlinkLayer);
-  document.getElementById("toggleEcotelButton").addEventListener("click", toggleEcotelLayer);
-  document.getElementById("toggleExeculinkButton").addEventListener("click", toggleExeculinkLayer);
-  document.getElementById("toggleSasktelButton").addEventListener("click", toggleSasktelLayer);
-  document.getElementById("toggleSogetelButton").addEventListener("click", toggleSogetelLayer);
+
+  // Add a geocoding control for location search
+  var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false
+  }).addTo(map);
+
+  // Location marker
+  var geocodeMarker = null;
+
+  // Function to remove the location marker with animation
+  function removeMarker(marker) {
+    if (marker) {
+      anime({
+        targets: marker._icon,
+        scale: 0,
+        duration: 500,
+        easing: 'easeInOutSine',
+        complete: function() {
+          map.removeLayer(marker);
+          geocodeMarker = null;
+        }
+      });
+    }
+  }
+
+  // Event to display the geocoding search result on the map
+  geocoder.on('markgeocode', function (e) {
+    var result = e.geocode;
+
+    // Remove the previous marker if it exists
+    removeMarker(geocodeMarker);
+
+    // Add the marker for the new location with animation
+    geocodeMarker = L.marker(result.center, { draggable: true, opacity: 0 }).addTo(map);
+
+    // Marker appearance animation
+    anime({
+      targets: geocodeMarker._icon,
+      opacity: 1,
+      duration: 200, // Reduce the duration to 200 (faster)
+      easing: 'easeInOutSine'
+    });
+
+    // Create a <span> element for the delete symbol
+    var deleteButton = document.createElement('span');
+    deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>'; // Use the desired Font Awesome CSS class and symbol
+
+    // Add custom CSS style if needed
+    deleteButton.style.cursor = 'pointer';
+
+    // Animation for hovering over the delete button
+    deleteButton.addEventListener('mouseover', function() {
+      anime({
+        targets: deleteButton,
+        rotate: '360deg',
+        duration: 50, // Reduce the duration to 50 (faster)
+        easing: 'easeInOutSine'
+      });
+    });
+
+    // Animation for clicking the delete button
+    deleteButton.addEventListener('click', function() {
+      anime({
+        targets: deleteButton,
+        scale: 0,
+        duration: 50, // Reduce the duration to 50 (faster)
+        easing: 'easeInOutSine',
+        complete: function() {
+          removeMarker(geocodeMarker);
+        }
+      });
+    });
+
+    // Add the delete button to the marker's popup
+    geocodeMarker.bindPopup(deleteButton);
+
+    // Handle marker dragstart event
+    geocodeMarker.on('dragstart', function (e) {
+      map.closePopup(); // Close any open popups when dragging the marker
+    });
+
+    geocodeMarker.on('dragend', function (e) {
+      var marker = e.target;
+      var position = marker.getLatLng();
+      console.log('New marker position:', position);
+    });
+
+    // Handle marker click event
+    geocodeMarker.on('click', function (e) {
+      var marker = e.target;
+      console.log('Marker clicked');
+      // Add your code here to handle marker click action (e.g., display a popup, etc.)
+    });
+  });
 });
